7f929d8ce4ae7c74e8c26005c0d94f1f17d9244d
Bhavana1801 <bhavana.manchana@gmail.com>
2016-03-18 14:34:29 +0530

    carousel added but changes should be made

--- js/index.js
+++ js/index.js
@@ -1,24 +1,9 @@
 
-angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'slick'])
 
 .controller('AppCtrl', function($scope) {
   $scope.imagePath = 'img/movies.jpg';
-  $scope.myDate = new Date();
-
-  $scope.minDate = new Date(
-      $scope.myDate.getFullYear(),
-      $scope.myDate.getMonth() - 2,
-      $scope.myDate.getDate());
-
-  $scope.maxDate = new Date(
-      $scope.myDate.getFullYear(),
-      $scope.myDate.getMonth() + 2,
-      $scope.myDate.getDate());
-  
-  $scope.onlyWeekendsPredicate = function(date) {
-    var day = date.getDay();
-    return day === 0 || day === 6;
-  }
+ 
 });
 
 
