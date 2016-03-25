c9a1f322208b4acd8ebe572eb7248d04561ae463
Bhavana1801 <bhavana.manchana@gmail.com>
2016-03-25 14:03:39 +0530

    button functionalities implemented

--- js/index.js
+++ js/index.js
@@ -3,7 +3,47 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', '
 
 .controller('AppCtrl', function($scope) {
   $scope.imagePath = 'img/movies.jpg';
- 
+  $scope.myDate = new Date();
+
+  $scope.minDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() - 2,
+      $scope.myDate.getDate());
+
+  $scope.maxDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() + 2,
+      $scope.myDate.getDate());
+  
+  $scope.onlyWeekendsPredicate = function(date) {
+    var day = date.getDay();
+    return day === 0 || day === 6;
+  }
+
+  $("#telugu-btn").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#telugu").offset().top
+    }, 2000);
+  });
+
+  $("#hindi-btn").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#hindi").offset().top
+    }, 2000);
+  });
+
+  $("#english-btn").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#english").offset().top
+    }, 2000);
+  });
+
+  $("#others-btn").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#others").offset().top
+    }, 1000);
+  });
+
 });
 
 
