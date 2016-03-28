96af64006bce30c595672b265eacf479570c9d84
Bhavana1801 <bhavana.manchana@gmail.com>
2016-03-28 20:04:27 +0530

    code with comments, without any slick components

--- js/index.js
+++ js/index.js
@@ -1,49 +1,33 @@
-
-angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'slick'])
+// controller for MyApp
+angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
 .controller('AppCtrl', function($scope) {
+  // image is loaded from the given location into the card
   $scope.imagePath = 'img/movies.jpg';
-  $scope.myDate = new Date();
-
-  $scope.minDate = new Date(
-      $scope.myDate.getFullYear(),
-      $scope.myDate.getMonth() - 2,
-      $scope.myDate.getDate());
-
-  $scope.maxDate = new Date(
-      $scope.myDate.getFullYear(),
-      $scope.myDate.getMonth() + 2,
-      $scope.myDate.getDate());
-  
-  $scope.onlyWeekendsPredicate = function(date) {
-    var day = date.getDay();
-    return day === 0 || day === 6;
-  }
-
-  $("#telugu-btn").click(function(){
+  // scrolled to telugu bar
+   $("#telugu-btn").click(function(){
     $('html,body').animate({
       scrollTop: $("#telugu").offset().top
-    }, 2000);
+    }, 1000);
   });
-
+   //scrolled to hindi movies list
   $("#hindi-btn").click(function(){
     $('html,body').animate({
       scrollTop: $("#hindi").offset().top
-    }, 2000);
+    }, 1000);
   });
-
+  //scrolled to english movies list
   $("#english-btn").click(function(){
     $('html,body').animate({
       scrollTop: $("#english").offset().top
-    }, 2000);
+    }, 1000);
   });
-
+  //scrolled to others movies list
   $("#others-btn").click(function(){
     $('html,body').animate({
       scrollTop: $("#others").offset().top
     }, 1000);
   });
-
 });
 
 
